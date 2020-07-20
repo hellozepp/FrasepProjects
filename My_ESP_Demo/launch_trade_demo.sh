6b4359ff345649978078e8b619824cda1aa0c159
frasep <sebastien.Poussart@laposte.net>
2020-07-20 09:56:33 +0200

    esp demo updated

---
+++ My_ESP_Demo/launch_trade_demo.sh
@@ -0,0 +1,6 @@
+. ./esp_env.sh
+
+./publish_trades_generator.sh 
+
+./launch_trade_generator.sh
+
---
+++ My_ESP_Demo/stop_trade_generator.sh
@@ -0,0 +1,4 @@
+. ./esp_env.sh
+
+dfesp_xml_client -url "https://frasepviya35smp:5556/SASESP/eventGenerators/tradesEG/state?value=stopped" -put
+
