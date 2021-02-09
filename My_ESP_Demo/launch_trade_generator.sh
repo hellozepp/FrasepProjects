701d5a6826a30d32b8516890120c9658a960a9ab
Viya Demo User 01 <viyademo01@frasepviya35smp.cloud.com>
2021-02-09 19:36:50 +0100

    update

--- My_ESP_Demo/launch_trade_generator.sh
+++ My_ESP_Demo/launch_trade_generator.sh
@@ -1,4 +1,4 @@
 . ./esp_env.sh
 
-dfesp_xml_client -url "https://frasepviya35smp:5556/SASESP/eventGenerators/tradesEG/state?value=started&events=1000000&blocksize=1&rate=100" -put
+dfesp_xml_client -url "https://frasepviya35smp:5556/SASESP/eventGenerators/tradesEG/state?value=started&events=1000000&blocksize=1&rate=10" -put
 
