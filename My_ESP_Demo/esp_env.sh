2779059c17661aa610f5f55626686741e0e99112
Viya Demo User 01 <viyademo01@frasepviya35smp.cloud.com>
2021-02-09 16:50:01 +0100

    update my esp demo (pb with newline windows)

--- My_ESP_Demo/esp_env.sh
+++ My_ESP_Demo/esp_env.sh
@@ -1,7 +1,5 @@
-export DFESP_HOME=/opt/sas/viya/home/SASEventStreamProcessingEngine/6.2 
+export DFESP_HOME=/opt/sas/viya/home/SASEventStreamProcessingEngine/6.2
 export LD_LIBRARY_PATH=$DFESP_HOME/lib:/opt/sas/viya/home/SASFoundation/sasexe
 export PATH=$PATH:$DFESP_HOME/bin
 export SV_ENABLE_DATA_SHARING=true
 export LD_LIBRARY_PATH=$DFESP_HOME/lib:/opt/sas/viya/home/SASFoundation/sasexe:$LD_LIBRARY_PATH
-dfesp_metering -d
-
