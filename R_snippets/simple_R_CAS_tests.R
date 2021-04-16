2c214a0de4234202a5f515861caff1ee734f05b8
Sebastien Poussart <sebastien.poussart@sas.com>
2021-04-16 09:05:11 +0200

    test swat

--- R_snippets/simple_R_CAS_tests.R
+++ R_snippets/simple_R_CAS_tests.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(reshape2)
 library(dplyr)
 
+Rprof()
+
 #Sys.setenv(CAS_CLIENT_SSL_CA_LIST = "C://Users/frasep/OneDrive - SAS/Mes Documents/Dossiers/Demoing/My_Local_Environments/frasepviya35smp/frasepviya35smp.cloud.com/cacerts/trustedcerts.pem")
 
 Sys.setenv(CAS_CLIENT_SSL_CA_LIST = "C://Users/frasep/OneDrive - SAS/Mes Documents/Dossiers/Demoing/My_Azure_Environments/frasepviya35smp/cacerts/trustedcerts.pem")
@@ -17,13 +19,7 @@ Sys.time()
 tbl <- defCasTable(conn, tablename="sample_350columns", caslib = "PUBLIC")
 tblR <- to.casDataFrame(tbl)
 Sys.time()
+# 23 min 
 
-
-Sys.time()
-tblR <- cas.table.fetch(conn,to=10000,table=list(name="sample_350columns",caslib = "PUBLIC", singlepass="true"));
-Sys.time()
-
-
-install.packages("https://github.com/sassoftware/R-swat/releases/download/v1.6.1-snapshot/R-swat-1.6.0.9000+vb20060-win-64.tar.gz", repos=NULL, type='file')
 
 cas.session.endSession(conn)
