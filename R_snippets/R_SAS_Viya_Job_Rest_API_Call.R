23fc0c28e938d1095f720f1c0c7de389184bc7d2
frasep <sebastien.poussart@laposte.net>
2021-10-14 11:24:13 +0200

    update

--- R_snippets/R_SAS_Viya_Job_Rest_API_Call.R
+++ R_snippets/R_SAS_Viya_Job_Rest_API_Call.R
@@ -1,7 +1,7 @@
 # *******************************************************
 # *******************************************************
-
 library(httr)
+library(jsonlite)
 
 # *******************************************************
 # Get oauth token for SAS Viya REST API calls
@@ -23,18 +23,39 @@ get_viya_oauth_token <- function(viyaBaseUri, authUri, clientid, clientsecret) {
 # Execute a SAS Viya job
 # ********************************************************
 execute_viya_job <- function(viyaBaseUri, fulljobname, parameterstr, access_token) {
-  jobcalluri <- paste(viyaBaseUri,"/SASJobExecution?_program=",fulljobname,parameterstr,sep="")
+  jobcalluri <- URLencode(paste(viyaBaseUri,"/SASJobExecution?_program=",fulljobname,parameterstr,sep=""))
+  print("Generated URL :")
+  print(jobcalluri)
   
-  rJson <- GET(url = jobcalluri, add_headers(.headers = c('Authorization : Bearer' = access_token)), verbose())
-  return(rJson)
+  rJson <- GET(url = jobcalluri, add_headers("Authorization"=paste("Bearer", access_token)), verbose())
+  
+  return(fromJSON(content(rJson,as="text")))
 }
 
-# Test program
+# ******************************************************************************
+# Example of freqTab proc wrapper function (calling viya job with json output)
+# ******************************************************************************
 
+compute_correlations_with_cas <- function(in_castablename, in_caslibname, freqtab_table_clause) {
+  r <- execute_viya_job(
+    viyaBaseUri='https://frasepviya35smp.cloud.com',
+    fulljobname='/Public/Job definition/Job_correlation',
+    parameterstr=paste('&castabname=',in_castablename,'&caslibname=',in_caslibname,'&tablesclause=',freqtab_table_clause,sep=""),
+    access_token=viya_access_token
+  )
+  return(r)
+}
+
+# ********************************************************
+# Test program
+# ********************************************************
+
+# Get Oauth token to access Viya REST API
 client_id <- rstudioapi::askForPassword()
 client_secret <- rstudioapi::askForPassword()
-
 viya_access_token <- get_viya_oauth_token('https://frasepviya35smp.cloud.com', '/SASLogon/oauth/token', client_id, client_secret)
 
-execute_viya_job('https://frasepviya35smp.cloud.com','%2FPublic%2FJob%20definition%2FJob_correlation','&castabname=churn&caslibname=PUBLIC&tablesclause=clage*job',viya_access_token)
+# Compute correlation with CAS
+r <- compute_correlations_with_cas('HMEQ_TRAIN', 'AZUREDL', 'clage * debtinc)')
 
+r[r['Statistic'] == 'Spearman Correlation']
