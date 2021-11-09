eed60cf2d732010230a3e1e7337f123ea6651bf2
frasep <sebastien.poussart@laposte.net>
2021-11-09 19:04:59 +0100

    update

--- R_snippets/Viya_Job_Rest_with_R_sample/R_SAS_Viya_Job_Rest_API_Call.R
+++ R_snippets/Viya_Job_Rest_with_R_sample/R_SAS_Viya_Job_Rest_API_Call.R
@@ -39,8 +39,22 @@ execute_viya_job <- function(viyaBaseUri, fulljobname, parameterstr, access_toke
 compute_freqtab <- function(in_castablename, in_caslibname, freqtab_table_clause, freqtab_table_options, freqtab_output_clause) {
   r <- execute_viya_job(
     viyaBaseUri='https://frasepviya35smp.cloud.com',
-    fulljobname='/Public/Job definition/Job_proc_freqtab',
+    fulljobname='/Production/Job_definition/Job_proc_freqtab',
     parameterstr=paste('&castabname=',in_castablename,'&caslibname=',in_caslibname,'&tablesclause=',freqtab_table_clause,'&table_options=',freqtab_table_options,'&output_options=',freqtab_output_clause,sep=""),
+    access_token=viya_access_token
+  )
+  return(r)
+}
+
+# ******************************************************************************
+# Example of freqTab proc wrapper function (calling viya job with json output)
+# ******************************************************************************
+
+compute_corr <- function(in_castablename, in_caslibname, corrclause, varclause, withclause) {
+  r <- execute_viya_job(
+    viyaBaseUri='https://frasepviya35smp.cloud.com',
+    fulljobname='/Production/Job_definition/Job_proc_corr',
+    parameterstr=paste('&castabname=',in_castablename,'&caslibname=',in_caslibname,'&corrclause=',corrclause,'&varclause=',varclause,'&withclause=',withclause,sep=""),
     access_token=viya_access_token
   )
   return(r)
@@ -56,6 +70,13 @@ client_secret <- rstudioapi::askForPassword()
 viya_access_token <- get_viya_oauth_token('https://frasepviya35smp.cloud.com', '/SASLogon/oauth/token', client_id, client_secret)
 
 # MEGACORP5_4m (4,5 million rows)
+
+# Compute spearman correlation on Viya with proc corr (2 continous variables of 91000000 distinct values : 228 secondes)
+starttime <- Sys.time()
+r <- compute_corr(in_castablename='test_corr', in_caslibname='public', corrclause='spearman pearson', varclause='nums1', withclause='nums2')
+endtime <- Sys.time()
+difftime(endtime, starttime, unit = "secs")
+r
 
 # Compute spearman correlation on Viya
 starttime <- Sys.time()
