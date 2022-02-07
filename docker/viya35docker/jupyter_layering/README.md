c190f1941c0359b8369de8719acf0761421c0f49
frasep <sebastien.poussart@sas.com>
2022-02-07 23:01:52 +0100

    updates

---
+++ docker/viya35docker/jupyter_layering/README.md
@@ -0,0 +1,1 @@
+https://documentation.sas.com/doc/en/calcdc/3.5/dplyml0phy0dkr/n1764pfqi6bc13n1mvibgybqzics.htm
---
+++ docker/viya35docker/jupyter_layering/post_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+###############################################################################
+# Run Jupyter notebook
+###############################################################################
+export SASSERVICENAME="jupyter"
+export JPY_COOKIE_SECRET=`openssl rand -hex 32`
+export SSLCALISTLOC="${SASHOME}/SASSecurityCertificateFramework/cacerts/trustedcerts.pem"
+export CAS_CLIENT_SSL_CA_LIST="/data/casconfig/sascas.pem"
+
+# create it here also (instead of just in sasdemo user creation)
+runuser --shell "/bin/sh" --login ${RUN_USER} \
+    --command "mkdir -p ~/jupyter"
+
+cp /usr/lib/python3.4/site-packages/saspy/sascfg.py\
+/usr/lib/python3.4/site-packages/saspy/sascfg_personal.py
+&& sed -i -e "s#/opt/sasinside/SASHome/SASFoundation\
+/9.4/bin/sas_u8#/opt/sas/spre/home/SASFoundation/sas#g"\
+ /usr/lib/python3.4/site-packages/saspy/sascfg_personal.py
+
+
+echo "Starting ${SASSERVICENAME}..."
+
+#docker_create_pid_file
+_jupyterpid=${DOCKERPIDFILE}
+
+# create jupyter config file
+runuser --shell "/bin/sh" --login ${RUN_USER} \
+    --command "jupyter notebook --generate-config"
+
+runuser --shell "/bin/sh" --login ${RUN_USER} \
+    --command "JPY_COOKIE_SECRET=${JPY_COOKIE_SECRET} \
+    SSLCALISTLOC=${SSLCALISTLOC} \
+    CAS_CLIENT_SSL_CA_LIST=${CAS_CLIENT_SSL_CA_LIST} \
+    jupyter notebook \
+    --ip='*' \
+    --no-browser \
+    --NotebookApp.token='${JUPYTER_TOKEN}' \
+    --NotebookApp.terminals_enabled=False \
+    --NotebookApp.base_url=/jupyter \
+    --KernelSpecManager.ensure_native_kernel=False \
+    --notebook-dir=~/jupyter &"
+
+# pgrep jupyter > ${_jupyterpid}
