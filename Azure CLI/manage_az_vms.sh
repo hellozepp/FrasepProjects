4432348cfff1f2ee32387f494db353c9a3d0888c
frasep <sebastien.poussart@sas.com>
2022-04-08 10:07:44 +0200

    Updates

---
+++ Azure CLI/manage_az_vms.sh
@@ -0,0 +1,10 @@
+az vm list --output table
+
+# Show details of a selection of vm in a specified resource group
+az vm list --resource-group FRASEPVIYA35MPP_RG --output table | cut -f1,3 -d" " | xargs -n2 sh -c 'az vm show --name "$0" --resource-group "$1"'
+
+# Deallocate a selection of vm (with specific resource group)
+az vm list --resource-group FRASEPVIYA35MPP_RG --output table | cut -f1,3 -d" " | xargs -n2 sh -c 'az vm deallocate --name "$0" --resource-group "$1" --verbose'
+
+# start a selection of vm
+az vm list --resource-group FRASEPVIYA35MPP_RG --output table | cut -f1,3 -d" " | xargs -n2 sh -c 'az vm start --name "$0" --resource-group "$1" --verbose'
